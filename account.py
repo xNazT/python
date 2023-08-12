867d81173c0adf8ea62f06b64ec8d2cc4357e512
xNazT <142054850+xNazT@users.noreply.github.com>
2023-08-11 19:44:40 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,12 +1,14 @@
 class Account:
-
+    
     def __init__(self, name):
 
         self.__account_name = name
         self.__account_balance = 0
 
     def deposit(self, amount: float) -> bool:
-
+        
+"""adds a specific amount to account balance"""
+        
         if amount > 0:
             self.__account_balance += amount
             return True
@@ -15,6 +17,8 @@ class Account:
 
     def withdraw(self, amount: float) -> bool:
 
+"""withdraw a specific amount from account balance"""
+        
         if amount > 0:
             if amount <= self.__account_balance:
                 self.__account_balance = self.__account_balance - amount
@@ -26,7 +30,13 @@ class Account:
             return False
 
     def get_balance(self) -> float:
+
+        """method to get the balance as a float"""
+        
         return self.__account_balance
 
     def get_name(self) -> str:
+
+        """method to get the account name as a string"""
+        
         return self.__account_name
